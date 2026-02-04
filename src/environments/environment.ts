7216a61c35da8aa55235a58b7f23321477746e37
Youssouph Faye <youssouph.faye@gmail.com>
2026-02-05 00:27:50 +0100

    Fast update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://server.ngelmak.org:4000/api',
-  apiUrl: 'https://server.ngelmak.org/api'
+  apiUrl: 'https://ngelmak.org/api'
 };
