34a0edbe6a9d2ae634b3a3cfc064ad4ba917f89f
Youssouph Faye <youssouph.faye@gmail.com>
2026-02-04 23:46:34 +0100

    Adding ngelmak server address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ngelmak.org:4000/api',
+  apiUrl: 'https://server.ngelmak.org:4000/api',
 };
-
-console.log('>>> USING PRODUCTION ENV FILE <<<');
