994732b7343d9b7aa53c9844b9d8d7259c487e9b
Youssouph Faye <youssouph.faye@gmail.com>
2026-06-28 13:47:24 +0200

    Image carousel update to support zoom

--- src/app/shared/image-carousel/image-carousel.component.ts
+++ src/app/shared/image-carousel/image-carousel.component.ts
@@ -1,20 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, signal } from '@angular/core';
-import { fadeInRight400ms } from '../animations/fade-in-right.animation';
-import { fadeInUp400ms } from '../animations/fade-in-up.animation';
-
 
 @Component({
   standalone: true,
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   imports: [CommonModule],
-  animations: [fadeInRight400ms, fadeInUp400ms],
 })
 export class ImageCarouselComponent {
   urls = input.required<string[]>();
 
   index = signal(0);
+  isLightboxOpen = signal(false);
+  lightboxIndex = signal(0);
 
   next() {
     if (this.index() < this.urls().length - 1) {
@@ -31,6 +29,38 @@ export class ImageCarouselComponent {
   goTo(position: number) {
     if (position >= 0 && position < this.urls().length) {
       this.index.set(position);
+    }
+  }
+
+  openLightbox(position: number) {
+    this.lightboxIndex.set(position);
+    this.isLightboxOpen.set(true);
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeLightbox() {
+    this.isLightboxOpen.set(false);
+    document.body.style.overflow = 'auto';
+  }
+
+  lightboxNext(event: Event) {
+    event.stopPropagation();
+    if (this.lightboxIndex() < this.urls().length - 1) {
+      this.lightboxIndex.update((i) => i + 1);
+    }
+  }
+
+  lightboxPrev(event: Event) {
+    event.stopPropagation();
+    if (this.lightboxIndex() > 0) {
+      this.lightboxIndex.update((i) => i - 1);
+    }
+  }
+
+  lightboxGoTo(position: number, event: Event) {
+    event.stopPropagation();
+    if (position >= 0 && position < this.urls().length) {
+      this.lightboxIndex.set(position);
     }
   }
 }
